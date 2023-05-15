76517f9b027faad38dbd2b8d65230390a0a10cec
Rafael William <rafael.fim@hotmail.com>
2023-05-15 10:55:08 -0300

    custom status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
@@ -6,6 +6,11 @@ export default function App() {
 
   return (
     <View>
+      <StatusBar 
+        barStyle={'light-content'}
+        backgroundColor='transparent'
+        translucent
+      />
       { fontsLoaded ? <Text>Hello World!</Text> : <View /> }
     </View>
   );
