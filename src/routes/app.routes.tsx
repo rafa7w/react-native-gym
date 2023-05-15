05558c30cdb62b6f360addd4a891162cdb08d0de
Rafael William <rafael.fim@hotmail.com>
2023-05-15 20:58:55 -0300

    home header

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native'
 import { useTheme } from 'native-base'
 import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 
@@ -22,13 +23,22 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
-  const { sizes } = useTheme()
+  const { sizes, colors } = useTheme()
   const iconSizes = sizes[6]
 
   return (
     <Navigator screenOptions={{
       headerShown: false,
-      tabBarShowLabel: false
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: colors.green[500],
+      tabBarInactiveTintColor: colors.gray[200],
+      tabBarStyle: {
+        backgroundColor: colors.gray[600],
+        borderTopWidth: 0,
+        height: Platform.OS === 'android' ? 'auto' : 96,
+        paddingBottom: sizes[10],
+        paddingTop: sizes[6]
+      }
     }}>
       <Screen 
         name='home'
@@ -60,6 +70,9 @@ export function AppRoutes() {
       <Screen 
         name='exercise'
         component={Exercise}
+        options={{
+          tabBarButton: () => null
+        }}
       />
     </Navigator>
   )
