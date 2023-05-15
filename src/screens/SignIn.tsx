9d26f7fd8125dadab8a787651be3f09ffd0ba25f
Rafael William <rafael.fim@hotmail.com>
2023-05-15 14:27:20 -0300

    apply public navigation

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,18 @@
+import { useNavigation } from '@react-navigation/native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  function handleNewAccount() {
+    navigation.navigate('signUp')
+  }
+
   return (
     <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
 
@@ -50,6 +58,7 @@ export function SignIn() {
           <Button 
             title='Criar conta'
             variant='outline'
+            onPress={handleNewAccount}
           />
         </Center>
       </VStack>
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
@@ -5,6 +6,12 @@ import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
 export function SignUp() {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
     <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
 
@@ -49,6 +56,7 @@ export function SignUp() {
           title='Voltar para o login'
           variant='outline'
           mt={24}
+          onPress={handleGoBack}
         />
         
       </VStack>
