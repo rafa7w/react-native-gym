e7f4e14cddae7f3859ce84cbbf1d0d14437c632e
Rafael William <rafael.fim@hotmail.com>
2023-05-15 13:55:04 -0300

    button variant

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading } from 'native-base'
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import { Input } from '@components/Input'
@@ -6,39 +6,53 @@ import { Button } from '@components/Button'
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg='gray.700' px={10}>
-      <Image 
-        source={BackgroundImg}
-        alt='Pessoas treinando'
-        resizeMode='contain'
-        position='absolute'
-      />
-      <Center my={24}>
-        <LogoSvg />
-        <Text color='gray.100' fontSize='sm'>
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
-      
-      <Center>
-        <Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
-          Acesse susa conta
-        </Heading>
+    <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
 
-        <Input 
-          placeholder='E-mail'
-          keyboardType='email-address'
-          autoCapitalize='none' // teclado com letras minúsculas
+      <VStack flex={1} bg='gray.700' px={10} pb={16}>
+        <Image 
+          source={BackgroundImg}
+          alt='Pessoas treinando'
+          resizeMode='contain'
+          position='absolute'
         />
-        <Input
-          placeholder='Senha'
-          secureTextEntry
-        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color='gray.100' fontSize='sm'>
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+        
+        <Center>
+          <Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
+            Acesse susa conta
+          </Heading>
 
-        <Button 
-          title='Acessar'
-        />
-      </Center>
-    </VStack>
+          <Input 
+            placeholder='E-mail'
+            keyboardType='email-address'
+            autoCapitalize='none' // teclado com letras minúsculas
+          />
+          <Input
+            placeholder='Senha'
+            secureTextEntry
+          />
+
+          <Button 
+            title='Acessar'
+          />
+        </Center>
+
+        <Center mt={24}>
+          <Text color='gray.100' fontSize='sm' mb={3} fontFamily='body'>
+            Ainda não tem acesso?
+          </Text>
+
+          <Button 
+            title='Criar conta'
+            variant='outline'
+          />
+        </Center>
+      </VStack>
+    </ScrollView>
   )
 }
