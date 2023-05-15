dba826030e63d6398a2a25cf742e851e5614d396
Rafael William <rafael.fim@hotmail.com>
2023-05-15 14:21:34 -0300

    navigation theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,21 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { useTheme, Box } from 'native-base'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
+  const nativeBaseTheme = useTheme()
+
+  const theme = DefaultTheme
+
+  // pode desestruturar colors do useTheme também
+  theme.colors.background = nativeBaseTheme.colors.gray[700]
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
+    // para garantir que uma tela branca não apareça entre as transições de tela, envolvemos com uma box
+    <Box flex={1} bg='gray.700'>
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   )
 }
