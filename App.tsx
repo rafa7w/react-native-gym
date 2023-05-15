a63fa91e872a2b91f20c643419cfc7f330936f8e
Rafael William <rafael.fim@hotmail.com>
2023-05-15 11:16:34 -0300

    first step with native base (component library)

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
-import { Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
 
   return (
-    <View>
+    <NativeBaseProvider>
       <StatusBar 
         barStyle={'light-content'}
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <Text>Hello World!</Text> : <View /> }
-    </View>
+      { fontsLoaded ? <View /> : <View /> }
+    </NativeBaseProvider>
   );
 }
