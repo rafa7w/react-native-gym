79d64660f035aea9418ce21e13d13d0532f43130
Rafael William <rafael.fim@hotmail.com>
2023-05-15 10:50:54 -0300

    app fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
     <View>
-      <Text>Hello World!</Text>
+      { fontsLoaded ? <Text>Hello World!</Text> : <View /> }
     </View>
   );
 }
