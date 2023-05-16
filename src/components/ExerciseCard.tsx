254fc05166a1c2264544556c3cd5712af5ebfc0d
Rafael William <rafael.fim@hotmail.com>
2023-05-15 21:42:15 -0300

    list of exercises

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,40 @@
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { HStack, Heading, Image, Text, VStack, Icon } from 'native-base'
+import { Entypo } from '@expo/vector-icons'
+
+type Props = TouchableOpacityProps & {
+  
+}
+
+export function ExerciseCard({...rest}:Props) {
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack bg='gray.500' alignItems='center' p={2} pr={4} rounded='md' mb={3}>
+        <Image 
+          source={{uri:'https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'}}
+          alt='Imagem do exercício'
+          w={16}
+          h={16}
+          rounded='md'
+          mr={4}
+          resizeMode='center'
+        />
+
+        <VStack flex={1}>
+          <Heading fontSize='lg' color='white'>
+            Remada unilateral
+          </Heading>
+          <Text fontSize='sm' color='gray.200' mt={1} numberOfLines={2}>
+            3 séries x 12 repetições
+          </Text>
+        </VStack>
+
+        <Icon 
+          as={Entypo}
+          name='chevron-thin-right'
+          color='gray.300'
+        />
+      </HStack>
+    </TouchableOpacity>
+  )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
-import { VStack, FlatList } from 'native-base'
+import { VStack, FlatList, HStack, Heading, Text } from 'native-base'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
+import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
   const [groups, setGroups] = useState(['costa', 'bíceps', 'tríceps', 'ombro'])
+  const [exercises, setExercises] = useState(['Puxada Frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra'])
   const [groupSelected, setGroupSelected] = useState('costas')
 
   return (
@@ -17,7 +19,7 @@ export function Home() {
         renderItem={({item}) => (
           <Group 
             name={item} 
-            isActive={groupSelected === item}   
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}   
             onPress={() => setGroupSelected(item)}     
           />
         )}
@@ -27,6 +29,27 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent='space-between' mb={5}>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+          <Text color='gray.200' fontSize='sm'>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{paddingBottom: 20}}
+        />
+      </VStack>
     </VStack>
   )
 }
