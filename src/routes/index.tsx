2bd054aad960f9790742fe4b9aac279347c100e4
Rafael William <rafael.fim@hotmail.com>
2023-05-16 20:21:59 -0300

    controlled inputs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,7 +15,7 @@ export function Routes() {
     // para garantir que uma tela branca não apareça entre as transições de tela, envolvemos com uma box
     <Box flex={1} bg='gray.700'>
       <NavigationContainer theme={theme}>
-        <AppRoutes />
+        <AuthRoutes />
       </NavigationContainer>
     </Box>
   )
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,11 +1,14 @@
 import { useNavigation } from '@react-navigation/native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import { useForm, Controller } from 'react-hook-form'
+
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
 export function SignUp() {
+  const { control } = useForm()
   const navigation = useNavigation()
 
   function handleGoBack() {
@@ -35,17 +38,57 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input 
-            placeholder='Nome'
+
+          <Controller 
+            control={control}
+            name='name'
+            render={({field: {onChange, value}}) => (
+              <Input 
+                placeholder='Nome'
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
-          <Input 
-            placeholder='E-mail'
-            keyboardType='email-address'
-            autoCapitalize='none' // teclado com letras minúsculas
+
+          <Controller 
+            control={control}
+            name='email'
+            render={({field: {onChange, value}}) => (
+              <Input 
+                placeholder='E-mail'
+                keyboardType='email-address'
+                autoCapitalize='none' // teclado com letras minúsculas
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
-          <Input
-            placeholder='Senha'
-            secureTextEntry
+
+          <Controller 
+            control={control}
+            name='password'
+            render={({field: {onChange, value}}) => (
+              <Input
+                placeholder='Senha'
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Controller 
+            control={control}
+            name='password_confirm'
+            render={({field: {onChange, value}}) => (
+              <Input
+                placeholder='Confirme a senha'
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
 
           <Button 
