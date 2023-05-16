95e4a5b3ee0023cda6ccdb317314a8ac4c9a3b9a
Rafael William <rafael.fim@hotmail.com>
2023-05-15 21:16:53 -0300

    groups scroll

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,32 @@
+import { useState } from 'react'
+import { VStack, FlatList } from 'native-base'
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack } from 'native-base'
 
 export function Home() {
+  const [groups, setGroups] = useState(['costa', 'bíceps', 'tríceps', 'ombro'])
+  const [groupSelected, setGroupSelected] = useState('costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <Group 
+            name={item} 
+            isActive={groupSelected === item}   
+            onPress={() => setGroupSelected(item)}     
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{px:8}}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
