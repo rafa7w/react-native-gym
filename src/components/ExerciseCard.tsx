97851793923c17fb66936794e7b20c10a4bd581b
Rafael William <rafael.furlan@inlog.com.br>
2023-05-19 11:11:34 -0300

    load image from backend

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,6 +1,7 @@
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
 import { HStack, Heading, Image, Text, VStack, Icon } from 'native-base'
 import { Entypo } from '@expo/vector-icons'
+import { api } from '@services/api'
 import { ExerciseDTO } from '@dtos/ExerciseDTO'
 
 type Props = TouchableOpacityProps & {
@@ -12,7 +13,7 @@ export function ExerciseCard({data, ...rest}:Props) {
     <TouchableOpacity {...rest}>
       <HStack bg='gray.500' alignItems='center' p={2} pr={4} rounded='md' mb={3}>
         <Image 
-          source={{uri:'https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'}}
+          source={{uri:`${api.defaults.baseURL}/exercise/thumb/${data.thumb}`}}
           alt='Imagem do exercício'
           w={16}
           h={16}
