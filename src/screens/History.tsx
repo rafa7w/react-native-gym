58abe9901b407bb6b6db87eb4fd881a06f300dfe
Rafael William <rafael.furlan@inlog.com.br>
2023-05-19 14:10:35 -0300

    show historic data

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,20 +1,43 @@
-import { useState } from 'react'
-import { Heading, VStack, SectionList, Text } from 'native-base'
+import { useCallback, useState } from 'react'
+import { Heading, VStack, SectionList, Text, useToast } from 'native-base'
+import { useFocusEffect } from '@react-navigation/native'
+
 import { ScreenHeader } from '@components/ScreenHeader'
 import { HistoryCard } from '@components/HistoryCard'
 
+import { AppError } from '@utils/AppError'
+import { api } from '@services/api'
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO'
+
 
 export function History() {
-  const [exercises, setExercises] = useState([
-    {
-      title: '26.08.22',
-      data: ['Puxada frontal', 'Remada unilateral']
-    },
-    {
-      title: '27.08.22',
-      data: ['Puxada frontal']
-    },
-  ])
+  const [isLoading, setIsLoading] = useState(true)
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([])
+
+  const toast = useToast()
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true)
+      const response = await api.get('/history')
+      setExercises(response.data)
+      
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError ? error.message : 'Não foi possível carregar o histórico.'
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchHistory()
+  }, []))
 
   return (
     <VStack flex={1}>
@@ -22,9 +45,9 @@ export function History() {
 
       <SectionList 
         sections={exercises }
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({item}) => (
-          <HistoryCard />
+          <HistoryCard data={item}/>
         )}
         renderSectionHeader={({section}) => (
           <Heading color='gray.100' fontSize='md' mt={10} mb={3} fontFamily='heading'>
